4a947a4151e33d401cbb8bf8c31cfaffb4df799d
Gamingwithgoose <goose@gamingwithgoose.com>
2025-05-08 17:00:25 -0500

    Fixed Eggs
    Eggs are no longer off screen and now are a blue tint if shiny. Effects are visible in the party with their icon, in the summary, icon displayed in the pc and on the left preview panel. IT WORKS!!!!

--- Plugins/Shiny Eggs/shinyeggs.rb
+++ Plugins/Shiny Eggs/shinyeggs.rb
@@ -4,38 +4,83 @@
 # Essentials Version: 21.1
 #===============================================================================
 
-module GameData
-  class Species
-    class << self
-      alias shinyegg_icon_filename_from_pokemon icon_filename_from_pokemon unless method_defined?(:shinyegg_icon_filename_from_pokemon)
+class PokemonIconSprite < Sprite
+  alias shinyegg_pokemon_eq pokemon=
+  def pokemon=(pkmn)
+    shinyegg_pokemon_eq(pkmn)
+    return if !pkmn
 
-      def icon_filename_from_pokemon(pkmn)
-        return shinyegg_icon_filename_from_pokemon(pkmn) unless pkmn&.egg?
-        return pkmn.shiny? ? "Graphics/Pokemon/Eggs/000shiny_icon" : "Graphics/Pokemon/Eggs/000_icon"
-      end
+    if pkmn.egg? && pkmn.shiny?
+      self.tone = Tone.new(-40, 0, 80)   # Blue tint
+    else
+      self.tone = Tone.new(0, 0, 0)
     end
   end
 end
+
 
 #===============================================================================
 # Shiny Egg Summary Sprite and Icon Display Fix
 # Pokémon Essentials v21.1 Compatible
 #===============================================================================
-class PokemonSprite < Sprite
-  alias_method :shinyegg_setPokemonBitmap, :setPokemonBitmap
 
-  def setPokemonBitmap(pkmn, back = false)
-    if pkmn&.egg?
-      path = pkmn.shiny? ? "Graphics/Pokemon/Eggs/000Shiny.png" : "Graphics/Pokemon/Eggs/000.png"
-      if File.exist?(path)
-        self.bitmap&.dispose
-        self.bitmap = Bitmap.new(path)
-        self.mirror = false
-        return
+class PokemonSummary_Scene
+  alias shinyegg_drawPage drawPage
+  def drawPage(page)
+    if @sprites["pokemon"] && @pokemon
+      @sprites["pokemon"].setPokemonBitmap(@pokemon)
+
+      # Apply a blue tint if the Pokémon is a shiny egg
+      if @pokemon.egg? && @pokemon.shiny?
+        @sprites["pokemon"].tone = Tone.new(-40, 0, 80)  # bluish tint
       else
-        echoln "[ShinyEggs] Missing summary image: #{path}"
+        @sprites["pokemon"].tone = Tone.new(0, 0, 0)  # normal
       end
     end
-    shinyegg_setPokemonBitmap(pkmn, back)
+
+    shinyegg_drawPage(page)
+  end
+end
+
+#========================================================================
+# Box Icon Sprite
+#========================================================================
+
+class PokemonBoxIcon < IconSprite
+  alias shinyegg_refresh refresh
+  def refresh
+    shinyegg_refresh
+    return unless @pokemon
+    if @pokemon.egg? && @pokemon.shiny?
+      self.tone = Tone.new(-40, 0, 80)  # Minty-cyan tint
+    else
+      self.tone = Tone.new(0, 0, 0)
+    end
+  end
+end
+
+#========================================================================
+# Box Icon Sprite
+#========================================================================
+
+class PokemonStorageScene
+  alias shinyegg_overlay_tint pbUpdateOverlay
+  def pbUpdateOverlay(selection, party = nil)
+    shinyegg_overlay_tint(selection, party)
+    # Apply tint to the left-side preview if it's a shiny egg
+    sprite = @sprites["pokemon"]
+    if sprite && !sprite.disposed? && sprite.visible
+      pkmn = nil
+      if @screen.pbHeldPokemon
+        pkmn = @screen.pbHeldPokemon
+      elsif selection >= 0
+        pkmn = (party) ? party[selection] : @storage[@storage.currentBox, selection]
+      end
+      if pkmn&.egg? && pkmn.shiny?
+        sprite.tone = Tone.new(-40, 0, 80)
+      else
+        sprite.tone = Tone.new(0, 0, 0)
+      end
+    end
   end
 end
